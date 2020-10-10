2ca5eeeecf94d087e23fecb41b125816b6c1cebe
MHH BRASSE <marco.brasse@oce.com>
2020-10-10 18:56:22 +0200

    version 1.3

--- inc/TPlot.h
+++ inc/TPlot.h
@@ -7,18 +7,16 @@ class TPlot
 {  
 
 public:  
-		TPlot(TRender& myRenderer);
+		TPlot(TRender* renderer);
 		~TPlot();
 
-		bool plot(int x, int y, int r, int g, int b);
+		void plot(int x, int y, int r, int g, int b);
 		void drawLine(int xb, int yb, int xe, int ye, int r, int g, int b);
-		void TPlot::drawCircle( int x0,int y0, int radius,int r,int g, int b );
-		void TPlot::drawEllipse( int x0,int y0, int x1, int y1, int r, int g, int b);
+		void drawCircle( int x0,int y0, int radius,int r,int g, int b );
+		void drawEllipse( int x0,int y0, int x1, int y1, int r, int g, int b);
 
 private:
-		COLORREF* colorBuffer;
-		int frameWidth;
-		int frameHeight;
+		TRender* mRenderer;
 };
 
 #endif
